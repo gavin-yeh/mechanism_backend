65582d9f9d5cc93fdb1d731cd4da7277db367fb3
twgavin <twgavin@ko-noha.com>
2023-06-26 00:07:53 +0800

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 9526;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
